dc00bc5985de7d08ea7a8a7ef5ba325f60ddd039
Prakash <prkjha25@gmail.com>
2019-05-07 22:38:35 +0530

    email validation from server side

--- app/Http/Controllers/ATGController.php
+++ app/Http/Controllers/ATGController.php
@@ -19,7 +19,7 @@ class ATGController extends Controller
    {
        $this->validate($request, [
         'name' => 'required',
-        'email' => 'required|email|unique:atg_form',
+        'email' => 'required|email|max:255|regex:/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/|unique:atg_form',
         'pincode' => 'required|max:6|min:6',
         ]);
        atg_form::create($request->all());
